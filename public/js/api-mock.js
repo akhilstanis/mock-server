aa05ea785e1d8fed99fa19192c21544385a67c48
soffolk <zlx.star@gmail.com>
2013-10-22 20:54:45 +0800

    use js from http://jsonsh.com/ for json input and validate

--- public/js/api-mock.js
+++ public/js/api-mock.js
@@ -10,9 +10,9 @@ jQuery(function($) {
   })
 
   $('#add-params').click(function() {
-    $('#create-api .params-value').append("<div class='col-lg-4'>" + 
+    $('#create-api .params-value').append("<div class='col-md-4'>" + 
           "<input type='text' placeholder='NAME' class='form-control' name='route[params_key][]'>" +
-          "</div><div class='col-lg-7'><input type='text' placeholder='VALUE' " + 
+          "</div><div class='col-md-7'><input type='text' placeholder='VALUE' " + 
           "class='form-control' name='route[params_value][]'></div>");
   })
 
